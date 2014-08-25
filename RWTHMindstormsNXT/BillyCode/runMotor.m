fd80affd6aa344ec9f0db4267a5d4fef6b8dcbb7
John Pidgeon <john.pidgeon@uqconnect.edu.au>
2014-08-25 10:57:33 +1000

    fuck you jono

--- RWTHMindstormsNXT/BillyCode/runMotor.m
+++ RWTHMindstormsNXT/BillyCode/runMotor.m
@@ -47,59 +47,21 @@ function valid = runMotor(degrees, h)
     
     if strcmp(str,'yes')
     
+        mA = NXTMotor('A','Power',speed1,'TachoLimit',degx,'ActionAtTachoLimit','HoldBrake');
+        mB = NXTMotor('B','Power',speed2,'TachoLimit',degy,'ActionAtTachoLimit','HoldBrake');
+        mC = NXTMotor('C','Power',speed3,'TachoLimit',degz,'ActionAtTachoLimit','HoldBrake');
+        mA.SendToNXT();
+        mB.SendToNXT();
+        mC.SendToNXT();
+        
+        
         % Run all three motors to each degree
-        NXC_MotorControl(0,speed1,degx, false,'Brake',true);
-        NXC_MotorControl(1,speed2,degy, false,'Brake',true);
-        NXC_MotorControl(2,speed3,degz, false,'Brake',true);
-
-        valid  = 1;
+        %NXC_MotorControl(0,speed1,degx, false,'Brake',true);
+        %NXC_MotorControl(1,speed2,degy, false,'Brake',true);
+        %NXC_MotorControl(2,speed3,degz, false,'Brake',true);
         
         
-        kp = 1;
-        ki = 0.01;
-        currentPos = [0 0 0];
-        error0sum = 0;
-        error1sum = 0;
-        error2sum = 0;
-        while 1
-            lastPos = currentPos;
-            [a b c] = getRTC(h);
-            currentPos = [a b c]
-            error0 = degx - currentPos(1,1);
-            error1 = degy - currentPos(1,2);
-            error2 = degz - currentPos(1,3);
-            error0sum = error0sum + error0;
-            error1sum = error1sum + error1;
-            error2sum = error2sum + error2;
-            
-            angle0 = kp*error0 + ki*error0sum;
-            angle1 = kp*error1 + ki*error1sum;
-            angle2 = kp*error2 + ki*error2sum;
-            
-            if (isequal(lastPos,currentPos)&&(currentPos(1,1)==degx)&&(currentPos(1,2)==degy)&&(currentPos(1,3)==degz))
-                break
-            end
-            
-            if angle0 < 0
-                NXC_MotorControl(0,-abs(speed1),abs(angle0), false,'Brake',true);
-            else
-                NXC_MotorControl(0,abs(speed1),angle0, false,'Brake',true);
-            end
-            if angle1 < 0
-                NXC_MotorControl(1,-abs(speed2),abs(angle1), false,'Brake',true);
-            else
-                NXC_MotorControl(1,abs(speed2),angle1, false,'Brake',true);
-            end
-            if angle2 < 0
-                NXC_MotorControl(2,-abs(speed2),abs(angle2), false,'Brake',true);
-            else
-                NXC_MotorControl(2,abs(speed2),angle2, false,'Brake',true);
-            end
-            %NXC_MotorControl(0,speed1,error0, false,'Brake',true);
-            %NXC_MotorControl(1,speed2,error1, false,'Brake',true);
-            %NXC_MotorControl(2,speed3,error2, false,'Brake',true);
-            
-        end
+        
     else
         return
     end
